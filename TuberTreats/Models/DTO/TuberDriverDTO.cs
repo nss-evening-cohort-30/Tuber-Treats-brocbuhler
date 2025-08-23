5c5d28519a7f100b988de4a66e426ee7ab884da2
Broc Buhler <brocbuhler@gmail.com>
2025-08-23 11:45:00 -0700

    Most of tuberOrders complete

--- TuberTreats/Models/DTO/TuberDriverDTO.cs
+++ TuberTreats/Models/DTO/TuberDriverDTO.cs
@@ -4,5 +4,5 @@ public class TuberDriverDTO
 {
   public int Id { get; set; }
   public string Name { get; set; }
-  public string TuberDelivers { get; set; }
+  public string TuberDeliveries { get; set; }
 }
